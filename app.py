49daee6474beb705d25b20705e88a7a40baeab83
Lakshya Kumar Singh <lakshyakumarsingh1@gmail.com>
2025-07-04 11:02:06 +0530

    the web app is ready

--- app.py
+++ app.py
@@ -23,5 +23,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1)) #apply the same scaling that we did while training the model
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text='The House Price Prediction is {}'.format(output))
+
 if __name__ == "__main__":
     app.run(debug=True) #debug=True will help us to see the errors in the console
